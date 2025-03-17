4a44f36361069707a6c82f673b807d07230e302b
Eduardo Diaz-Castaneda <eduard3@uci.edu>
2025-03-17 04:40:30 +0000

    Attempted to fix an output issue in print word ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -98,7 +98,7 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 void print_word_ladder(const vector<string>& ladder) { 
     if (ladder.empty()) { 
-        cout << "No word ladder found.";
+        cout << "No word ladder found." <<endl;
         return;
     }
     cout << "Word ladder found: ";
